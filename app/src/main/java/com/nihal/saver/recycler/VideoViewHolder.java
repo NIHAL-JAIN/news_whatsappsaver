67a4a2507fb2825367f531ccd857fafc3709af1a
nihal <nihal.tca1609071@tmu.ac.in>
2020-08-27 22:32:35 +0530

    first commit

---
+++ app/src/main/java/com/nihal/saver/recycler/VideoViewHolder.java
@@ -0,0 +1,28 @@
+package com.nihal.saver.recycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.nihal.saver.R;
+
+/**
+ * Created by umer on 01-May-18.
+ */
+
+public class VideoViewHolder extends RecyclerView.ViewHolder {
+
+
+    public ImageView imageView,imageViewCheck,imageViewPlay;
+
+
+    public VideoViewHolder(View view) {
+        super(view);
+
+
+        this.imageView = (ImageView) view.findViewById(R.id.imageView_wa_image);
+        this.imageViewCheck = (ImageView) view.findViewById(R.id.imageView_wa_checked);
+        this.imageViewPlay = (ImageView) view.findViewById(R.id.imageView_wa_play);
+
+    }
+}
